8cf424b61fbcb642c583fb8ff7c7f7c1d6babe02
edricjmendoza <edricjmendoza@icloud.com>
2022-10-10 21:27:24 -0700

    sp

--- sketch.js
+++ sketch.js
@@ -73,8 +73,8 @@ function randomizer(){
   if (cars[0]) {
     background(random(200,225));
     randomIndex = int(random(cars.length));
-    // text(`${cars[randomIndex].name} is inspect ${cars[randomIndex].color}`, 50, 50);
-    text(cars[randomIndex].name + " is " + cars[randomIndex].color, 50, 50);
+    text(`${cars[randomIndex].name} is inspect ${cars[randomIndex].color}`, 50, 50);
+    // text(cars[randomIndex].name + " is " + cars[randomIndex].color, 50, 50);
     cars.splice(randomIndex, 1);
   } else {
     background(random(200,225));
